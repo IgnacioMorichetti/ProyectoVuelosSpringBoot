28fcb120ee97c42374f3a75665d1ec13ff3f095f
Ignacio Morichetti Ramal <moricheetti@gmail.com>
2026-05-23 10:56:26 -0300

    Primer commit. Organización y estructura del proyecto

---
+++ src/main/java/com/example/vuelos/ProyectoVuelosApplication.java
@@ -0,0 +1,12 @@
+package com.example.vuelos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoVuelosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProyectoVuelosApplication.class, args);
+    }
+}
